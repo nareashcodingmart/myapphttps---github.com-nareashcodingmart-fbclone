884555afd824eb10c49a84a53aa6c9e826d89a2f
nareashcodingmart <nareashcodingmart@codingmart.com>
2023-02-13 14:56:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import './login.css';
+import logo from './fblogin.svg';
 function App() {
+  const [emailfocus, setEmailfocus] = useState(false)
+  const [passwordfocus, setPasswordfocus] = useState(false)
+  const getvalue=()=>{
+    let a=document.getElementsByName("id");
+    console.log(a[0].value,a[1].value);
+    alert(a[0].value+" "+a[1].value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="outer">
+      <div id="leftcontainer">
+        <img src={logo} alt="logo" />
+        <h2>Facebook helps you connect and share with the people in your life.</h2>
+      </div>
+      <div id="rightcontainer">
+
+        <form >
+          <input onFocus={() => setEmailfocus(true)}
+            onBlur={() => setEmailfocus(false)}
+            id={emailfocus ? "active" : ""}
+            type="text" placeholder='Email address or phone number' name='id'></input>
+
+
+          <input onFocus={() => setPasswordfocus(true)}
+            onBlur={() => setPasswordfocus(false)}
+            id={passwordfocus ? "active" : ""}
+             name="id" type="password" placeholder='Password'></input>
+
+          <button id="logbutton" onClick={getvalue} >Log in </button>
+          <a href="">Forgotten password?</a>
+          <div id="line"></div>
+          <button id="createbutton">Create new account</button>
+        </form>
+        <div id="page"><b>Create a Page</b> for a celebrity, brand or business.</div>
+      </div>
     </div>
   );
 }
